8f88dcd7ea0502672fa17b3489edfba15caef186
Akjxc <60183793+Akjxc@users.noreply.github.com>
2024-07-23 00:45:10 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240723/a 부터 b 까지/a-to-b.cpp
+++ 240723/a 부터 b 까지/a-to-b.cpp
@@ -5,21 +5,14 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    
-
-    do {
+    while(a <= b){
         cout << a << " ";
-
         if(a % 2 == 0){
             a += 3;
         }
         else {
             a *= 2;
         }
-
-        if(a > b){
-            return 0;
-        }
-    } while (a <= b);
+    }
     return 0;
 }
